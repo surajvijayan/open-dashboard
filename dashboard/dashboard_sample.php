a2022fd3136a49d9aec109db3ea1c053383cbdc2
Suraj Vijayan <suraj.vijayan1966@gmail.com>
2018-09-14 15:22:37 -0700

    u

--- dashboard/dashboard_sample.php
+++ dashboard/dashboard_sample.php
@@ -3,7 +3,7 @@
 	Open-Dashboard: Designed by Suraj Vijayan - First Release: August 2018
 */
 session_start();
-include_once('../vars.inc');
+include_once('$_SERVER["DOCUMENT_ROOT"]/open-dashboard/vars.inc');
 require_once('./dashboard.php');
 require_once('./dashboard_sample.inc');
 print_header('dashboard_sample.js','sample_widgets.js');
